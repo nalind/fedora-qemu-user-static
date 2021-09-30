639de8ce61ad1d9ef1fb1bf6fef3c03a65fb452a
Nalin Dahyabhai <nalin@redhat.com>
2021-09-30 17:15:05 -0400

    Tag builds by date
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,4 +4,5 @@ IMAGE=${IMAGE:-ghcr.io/${GITHUBUSER}/fedora-qemu-user-static}
 SOURCE=${SOURCE:-https://github.com/${GITHUBUSER}/fedora-qemu-user-static}
 buildah rmi -f ${IMAGE} || true
 buildah bud --build-arg IMAGE="${IMAGE}" --build-arg SOURCE="${SOURCE}" --all-platforms --manifest ${IMAGE} .
+buildah manifest push --all "${IMAGE}" docker://"${IMAGE}":$(TZ=UTC date +%Y-%m-%d)
 buildah manifest push --all "${IMAGE}" docker://"${IMAGE}"
