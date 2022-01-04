68689fe2194171087ae266ba9a6ded14edf50668
Nalin Dahyabhai <nalin@redhat.com>
2022-01-04 15:10:48 -0500

    See how much we can do with a privileged daemonset
    ... in OpenShift, where we would typically not be allowed to do things
    because of the node's SELinux policy.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 check() {
 	if ! test -w /proc/sys/fs/binfmt_misc/register; then
+		# kernel facility not already available; hope it's a module that we can force to be loaded
 		mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc || true
 	fi
 	if ! test -w /proc/sys/fs/binfmt_misc/register; then
+		# kernel facility still not available; hope it's a module that we could force to be loaded
 		echo No /proc/sys/fs/binfmt_misc/register in container.  Please \`modprobe binfmt_misc\` and run with --privileged.
 		exit 1
 	fi
@@ -12,11 +14,21 @@ check() {
 register() {
 	for binfmt in /usr/lib/binfmt.d/* ; do
 		name=$(cut -f2 -d: ${binfmt})
+		binary=$(cut -f7 -d: ${binfmt})
 		echo -n "${name}: "
 		if test -r /proc/sys/fs/binfmt_misc/${name} ; then
+			# clear previous registration
 			echo -n -1 > /proc/sys/fs/binfmt_misc/${name} 
 		fi
-		cat ${binfmt} > /proc/sys/fs/binfmt_misc/register && echo ok
+		if test -n "${BINDIR}" ; then
+			# optionally copy this binary to a volume
+			install -v "${binary}" "${BINDIR}/"
+			if test -n "${CHCON}" ; then
+				# optionally try to relabel the binary in the volume
+				chcon -v ${CHCON} "${BINDIR}/${binary##*/}" || :
+			fi
+		fi
+		sed "s#/usr/bin#${BINDIR:-/usr/bin}#g" ${binfmt} > /proc/sys/fs/binfmt_misc/register && echo ok
 	done
 }
 unregister() {
